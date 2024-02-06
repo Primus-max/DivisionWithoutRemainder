bacf76a10222aa836412e6af7f85dbaa58c62b46
Vladimir <79203161+Primus-max@user.noreply.github.com>
2024-02-06 09:37:46 +0300

    Рефактор

--- DivisionWithoutRemainder.cpp
+++ DivisionWithoutRemainder.cpp
@@ -12,12 +12,23 @@ int main()
 
     int userInput = 0;
     cout << "Введите число и узнаете все числа на которое оно делится без остатка : ";
-    cin >> userInput;
 
-    for (int i = 1; i <= userInput; i++)
-    {
-        if (userInput % i == 0)
-            cout << "Данное число " << i << " делится на " << userInput << " без остатка" << endl;
+    while (true)
+    {        
+
+        if (!(cin >> userInput) || userInput <= 0)
+        {
+            cout << "Число или цифра не могу быть отрицательным или равным 0, попробуйте ещё раз : ";
+            continue;
+        }
+
+        for (int i = 1; i <= userInput; i++)
+        {
+            if (userInput % i == 0)
+                cout << "Данное число " << i << " делится на " << userInput << " без остатка" << endl;
+         
+            if (i == userInput) return 0;
+        }       
     }
 }
 
