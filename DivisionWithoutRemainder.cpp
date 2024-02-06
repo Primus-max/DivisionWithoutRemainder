2fc84b2984d2097b14a62985ac0a983b586bfe0e
Vladimir <79203161+Primus-max@user.noreply.github.com>
2024-02-06 09:24:08 +0300

    Добавьте файлы проекта.

---
+++ DivisionWithoutRemainder.cpp
@@ -0,0 +1,23 @@
+﻿// Пользователь вводит целое число. Необходимо вывести все целые числа, на которое заданное число делиться без остатка.
+
+
+#include <iostream>
+#include <windows.h>
+using namespace std;
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+
+    int userInput = 0;
+    cout << "Введите число и узнаете все числа на которое оно делится без остатка : ";
+    cin >> userInput;
+
+    for (int i = 1; i <= userInput; i++)
+    {
+        if (userInput % i == 0)
+            cout << "Данное число " << i << " делится на " << userInput << " без остатка" << endl;
+    }
+}
+
